58508ac40f73938b1b31635adff43e68272f975c
Artemxp <artem.ice33@gmail.com>
2017-11-01 12:29:52 +0300

    fix

--- bs/script.js
+++ bs/script.js
@@ -27,16 +27,28 @@ $(function () {
 
     var iframeVideo = $('.wrapper-player');
     $('.play').on('click', function(){
-
         var attr = $(this).attr('data-video');
-
         $('iframe').attr('src', attr);
         p.open(iframeVideo.html());
+        resizeIframe();
     });
     console.log($('.close img'));
 
+    $('.multiple-items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    var attr1 = [];
+    $('.play').each(function (){
+        attr1.push($(this).attr('data-video').slice(-11));
+    });
+    var i = 0;
+    $('.item1').each(function () {
+        $(this).css('background', 'linear-gradient(rgba(0,0,0, 1), rgba(0, 0, 0, 0)),'+'url(https://img.youtube.com/vi/'+attr1[i++]+'/maxresdefault.jpg) center no-repeat');
 
-
+    });
     $('.button-call-me').on('click', function (){
         var a = $('.wrapper-call-me');
         p.open(a.html());
@@ -44,12 +56,13 @@ $(function () {
             p.close();
         });
     });
+
     function resizeIframe(){
         $('iframe').each(function() {
             var width = $(this).width ();
-            $(this).css("height", width / 1.7777 + "px");
+            $(this).css("height", width / 2.0777 + "px");
         });
     };
     $(window).resize(resizeIframe);
-    resizeIframe();
+
 });
